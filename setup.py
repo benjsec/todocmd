49f4e818d6beee72c35c6650532ed32827c3e501
Ben Secretan <benjsec@gmail.com>
2016-05-25 15:15:57 +0100

    Add setup and installation files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='todo',
+    version='0.0.1',
+    description="Quick add task to todoist",
+    packages=['todo'],
+    author="Ben Secretan",
+    author_email='benjsec@gmail.com'
+)
